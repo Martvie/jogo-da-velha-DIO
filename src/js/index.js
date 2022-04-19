07489760df214d16a8fbba128114e6452c5944ae
Martvie <marviedias@gmail.com>
2022-04-19 14:45:12 -0300

    change player

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+var player = null
+var actualplayer =  document.getElementById('select-player')
+
+let squares = document.querySelectorAll('.square')
+
+changePlayer('X')
+
+function selectSquare(id){
+    let square = document.getElementById(id)
+
+    square.innerHTML = player
+
+    if(player === 'X'){
+        player = 'O'
+    } else {
+        player = 'X'
+    }
+
+    changePlayer(player)
+}
+
+function changePlayer(value) {
+    player = value
+    actualplayer.innerHTML = player
+}
