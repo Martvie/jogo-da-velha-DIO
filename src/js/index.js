122b6c73be693c2c829e76527d113c49a5e2ed38
Martvie <marviedias@gmail.com>
2022-04-20 22:19:46 -0300

    color square

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-var player = null
+var player, winner = null
 var actualplayer =  document.getElementById('select-player')
+var allsquares = document.getElementsByClassName('square')
 
 let squares = document.querySelectorAll('.square')
 
@@ -18,9 +19,28 @@ function selectSquare(id){
     }
 
     changePlayer(player)
+    verifyWinner(allsquares[0], allsquares[1], allsquares[2])
+
+
 }
 
 function changePlayer(value) {
     player = value
     actualplayer.innerHTML = player
+}
+
+function verifyWinner(square1, square2, square3){
+    endGame = false
+
+    if (square1.innerHTML != '.' && square1.innerHTML === square2.innerHTML && square2.innerHTML === square3.innerHTML){
+        endGame = true
+
+        changeColor(square1, square2, square3)
+    }
+}
+
+function changeColor(square1, square2, square3){
+    square1.style.backgroundColor = '#0e0'
+    square2.style.backgroundColor = '#0e0'
+    square3.style.backgroundColor = '#0e0'
 }
