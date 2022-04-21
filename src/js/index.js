f275c3cdd614dba502cee54f1fe4727bc816eba5
Martvie <marviedias@gmail.com>
2022-04-21 16:23:18 -0300

    game ok

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,26 @@
 var player, winner = null
-var actualplayer =  document.getElementById('select-player')
+var actualplayer = document.getElementById('select-player')
 var playerWinner = document.getElementById('winner-player')
+var winnerArea = document.getElementById('winner-area')
 var squares = document.getElementsByClassName('square')
 
 console.log(playerWinner)
 
 changePlayer('X')
 
-function selectSquare(id){
+function selectSquare(id) {
     let square = document.getElementById(id)
+
+    if (winner != null) {
+        return
+    }
+
+    if (square.innerHTML === '.'){
 
     square.innerHTML = player
     square.style.color = '#000'
 
-    if(player === 'X'){
+    if (player === 'X') {
         player = 'O'
     } else {
         player = 'X'
@@ -22,49 +29,50 @@ function selectSquare(id){
     changePlayer(player)
     verifyWinner(squares)
 }
+}
 
 function changePlayer(value) {
     player = value
     actualplayer.innerHTML = player
 }
 
-function verifyWinner(squares){
+function verifyWinner(squares) {
 
-    if (checkSequence(squares[0], squares[1], squares[2])){
+    if (checkSequence(squares[0], squares[1], squares[2])) {
         changeColor(squares[0], squares[1], squares[2])
         gameWinner(squares[0])
         return
-    } else if (checkSequence(squares[3], squares[4], squares[5])){
+    } else if (checkSequence(squares[3], squares[4], squares[5])) {
         changeColor(squares[3], squares[4], squares[5])
         gameWinner(squares[0])
         return
-    } else if (checkSequence(squares[6], squares[7], squares[8])){
+    } else if (checkSequence(squares[6], squares[7], squares[8])) {
         changeColor(squares[6], squares[7], squares[8])
         gameWinner(squares[6])
         return
-    } else if (checkSequence(squares[0], squares[3], squares[6])){
+    } else if (checkSequence(squares[0], squares[3], squares[6])) {
         changeColor(squares[0], squares[3], squares[6])
         gameWinner(squares[0])
         return
-    } else if (checkSequence(squares[1], squares[4], squares[7])){
+    } else if (checkSequence(squares[1], squares[4], squares[7])) {
         changeColor(squares[1], squares[4], squares[7])
         gameWinner(squares[1])
         return
-    } else if (checkSequence(squares[2], squares[5], squares[8])){
+    } else if (checkSequence(squares[2], squares[5], squares[8])) {
         changeColor(squares[2], squares[5], squares[8])
         gameWinner(squares[2])
         return
-    } else if (checkSequence(squares[0], squares[4], squares[8])){
+    } else if (checkSequence(squares[0], squares[4], squares[8])) {
         changeColor(squares[0], squares[4], squares[8])
         gameWinner(squares[0])
         return
-    } else if (checkSequence(squares[6], squares[4], squares[2])){
+    } else if (checkSequence(squares[6], squares[4], squares[2])) {
         changeColor(squares[6], squares[4], squares[2])
         gameWinner(squares[6])
     }
 }
 
-function changeColor(square1, square2, square3){
+function changeColor(square1, square2, square3) {
     square1.style.backgroundColor = '#0e0'
     square2.style.backgroundColor = '#0e0'
     square3.style.backgroundColor = '#0e0'
@@ -73,14 +81,23 @@ function changeColor(square1, square2, square3){
 function gameWinner(square) {
     winner = square.innerHTML
 
-    winnerArea = document.getElementById('winner-area')
-
     winnerArea.innerHTML = 'O vencedor é:'
     playerWinner.innerHTML = winner
 }
 
-function checkSequence (square1, square2, square3) {
-    if (square1.innerHTML != '.' && square1.innerHTML === square2.innerHTML && square2.innerHTML === square3.innerHTML){
+function checkSequence(square1, square2, square3) {
+    if (square1.innerHTML != '.' && square1.innerHTML === square2.innerHTML && square2.innerHTML === square3.innerHTML) {
         return true
+    }
+}
+
+function restartGame() {
+    winner = null
+    winnerArea.innerHTML , playerWinner.innerHTML = ''
+
+    for (let i = 0; i <= squares.length; i++){
+        squares[i].innerHTML = '.'
+        squares[i].style.backgroundColor = '#EEE'
+        squares[i].style.color = '#EEE'
     }
 }
